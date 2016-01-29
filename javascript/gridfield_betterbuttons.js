8675fb2cf3c3b95e19fd0736a3957e22c7790539
Nivanka Fonseka <nivanka@silverstripers.com>
2016-01-29 10:06:36 +0530

    added multi class selection options to the grid add new buttons. works in line with the GridFieldExtensions module

--- javascript/gridfield_betterbuttons.js
+++ javascript/gridfield_betterbuttons.js
@@ -5,9 +5,23 @@ $.entwine('ss', function($) {
 
 	$('.better-buttons-utils button').entwine({
 		onclick: function(e) {
-			$('.cms-container').submitForm($('#Form_ItemEditForm'), this);
+            if(!$(this).hasClass('ui-state-disabled')){
+                $('.cms-container').submitForm($('#Form_ItemEditForm'), this);
+            }
+            e.preventDefault();
 		}
 	});
+
+    $('.better-buttons-utils select.add-new-selected').entwine({
+        onchange : function(e){
+            if(this.val() == ""){
+                this.parent().find('button').addClass('ui-state-disabled ssui-state-disabled');
+            }
+            else {
+                this.parent().find('button').removeClass('ui-state-disabled').removeClass('ssui-state-disabled');
+            }
+        }
+    });
 
 	$('.cms form#Form_ItemEditForm .better-button-nested-form').entwine({
 		onclick: function (e) {
